748e15ac1f4246e7605cd9b4573df3b138dd379f
Ricardo Dias <slowmotion@outlook.com>
2024-06-14 17:38:57 +0100

    Refactured jobs view to it's own folder

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,19 @@
+<x-layout heading="About">
+    <x-slot:heading>Job Listings</x-slot:heading>
+    <div class="space-y-4">
+        @foreach($jobs as $job)
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500">
+                    {{$job->employer->name}}
+                </div>
+                <div class="flex">
+                    <span class="font-bold ">{{$job['title']}}:</span>
+                    Pays {{$job['salary']}} per year.
+                </div>
+            </a>
+        @endforeach
+        <div>
+            {{$jobs->links()}}
+        </div>
+    </div>
+</x-layout>
